cc48757c01ff37628e6107d95cc8aea515b593d2
Raphaël <raphaos.rb@gmail.com>
2024-05-11 22:49:10 +0200

    Update GameBoard

--- src/fr/esiee/babaisyou/Main.java
+++ src/fr/esiee/babaisyou/Main.java
@@ -1,7 +1,21 @@
 package fr.esiee.babaisyou;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Bienvenue sur ce projet");
-  }
+    public static void main(String[] args) {
+        System.out.println("Bienvenue sur ce projet");
+
+        GameBoard g1 = new GameBoard(6,6);
+        g1.displayBoard();
+        System.out.println();
+        g1.swap(0,0,2,2);
+        g1.displayBoard();
+        System.out.println();
+        g1.swap(0,0, 2,5);
+        g1.displayBoard();
+        System.out.println();
+        g1.swap(2,5, 1,1);
+        g1.displayBoard();
+
+    }
+
 }
